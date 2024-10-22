1867c6db142f4bf00beb8f708a3d6637c09797c3
MrJonatans <145282159+MrJonatans@users.noreply.github.com>
2024-10-22 17:52:06 +0300

    add basic methods + Iterator override

--- src/main/java/org/container/Container.java
+++ src/main/java/org/container/Container.java
@@ -1,9 +1,10 @@
 package org.container;
+import java.util.Iterator;
 
 /**
  * A container for storing an arbitrary number of objects.
  */
-public class Container {
+public class Container implements Iterable<Object> {
 
     static private class Node {
         Object data;
@@ -93,4 +94,137 @@ public class Container {
     public int size() {
         return size;
     }
+
+    /**
+     * Returns a string representation of the container. The string representation
+     * consists of a list of the container's elements, enclosed in square brackets
+     * ("[]"). Adjacent elements are separated by the characters ", " (comma and
+     * space).
+     * Example:
+     * <pre>
+     *     Container container = new Container();
+     *     container.add("A");
+     *     container.add("B");
+     *     System.out.println(container); // Outputs: [A, B]
+     * </pre>
+     *
+     * @return a string representation of the container and its elements
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node current = head;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    /**
+     * Compares the specified object with this container for equality. Returns
+     * {@code true} if and only if the specified object is also a container,
+     * both containers have the same size, and all corresponding pairs of elements
+     * in the two containers are equal.
+     * Two containers are considered equal if they have the same elements in the
+     * same order.
+     *
+     * @param obj the object to be compared for equality with this container
+     * @return {@code true} if the specified object is equal to this container,
+     *         {@code false} otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Container other = (Container) obj;
+        if (this.size != other.size) {
+            return false;
+        }
+        Node currentThis = this.head;
+        Node currentOther = other.head;
+        while (currentThis != null) {
+            if (!currentThis.data.equals(currentOther.data)) {
+                return false;
+            }
+            currentThis = currentThis.next;
+            currentOther = currentOther.next;
+        }
+        return true;
+    }
+
+    /**
+     * Returns the hash code value for this container. The hash code of a container
+     * is defined as the sum of the hash codes of its elements, where the hash code
+     * of a {@code null} element is defined to be 0.
+     * The formula used for calculating the hash code is:
+     * <pre>
+     *     int hashCode = 1;
+     *     for (Object element : container) {
+     *         hashCode = 31 * hashCode + (element == null ? 0 : element.hashCode());
+     *     }
+     * </pre>
+     *
+     * This ensures that {@code container1.equals(container2)} implies that
+     * {@code container1.hashCode() == container2.hashCode()}.
+     *
+     * @return the hash code value for this container
+     */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        Node current = head;
+        while (current != null) {
+            result = 31 * result + (current.data == null ? 0 : current.data.hashCode());
+            current = current.next;
+        }
+        return result;
+    }
+
+    /**
+     * Returns an iterator over the elements in this container in proper sequence.
+     * The returned iterator will iterate over the elements in the order in which
+     * they were added to the container.
+     * Example usage:
+     * <pre>
+     *     Container container = new Container();
+     *     container.add("A");
+     *     container.add("B");
+     *     for (Object item : container) {
+     *         System.out.println(item); // Outputs: A, B
+     *     }
+     * </pre>
+     *
+     * @return an iterator over the elements in this container
+     */
+    @Override
+    public Iterator<Object> iterator() {
+        return new ContainerIterator();
+    }
+
+    private class ContainerIterator implements Iterator<Object> {
+        private Node current = head;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Object next() {
+            Object data = current.data;
+            current = current.next;
+            return data;
+        }
+    }
+
 }
--- src/test/java/ContainerTest.java
+++ src/test/java/ContainerTest.java
@@ -48,4 +48,61 @@ class ContainerTest {
     void testRemoveOutOfBounds() {
         assertThrows(IndexOutOfBoundsException.class, () -> container.remove(0), "Должно выбросить IndexOutOfBoundsException");
     }
+    @Test
+    public void testRemoveThrowsIndexOutOfBoundsException() {
+        Container container = new Container();
+        container.add("First");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            container.remove(1); // индекс вне диапазона
+        }, "Expected IndexOutOfBoundsException when removing invalid index");
+    }
+
+    @Test
+    public void testToString() {
+        Container container = new Container();
+        assertEquals("[]", container.toString(), "Empty container should return '[]'");
+
+        container.add("First");
+        assertEquals("[First]", container.toString(), "Container should return '[First]' after adding 'First'");
+
+        container.add("Second");
+        assertEquals("[First, Second]", container.toString(), "Container should return '[First, Second]' after adding two elements");
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        Container container1 = new Container();
+        Container container2 = new Container();
+
+        // Оба контейнера пусты, должны быть равны
+        assertEquals(container1, container2, "Two empty containers should be equal");
+        assertEquals(container1.hashCode(), container2.hashCode(), "Hash codes of two empty containers should be equal");
+
+        container1.add("First");
+        container2.add("First");
+
+        assertEquals(container1, container2, "Containers with same elements should be equal");
+        assertEquals(container1.hashCode(), container2.hashCode(), "Hash codes of containers with same elements should be equal");
+
+        container2.add("Second");
+
+        assertNotEquals(container1, container2, "Containers with different elements should not be equal");
+        assertNotEquals(container1.hashCode(), container2.hashCode(), "Hash codes should not match for different containers");
+    }
+
+    @Test
+    public void testIterable() {
+        Container container = new Container();
+        container.add("First");
+        container.add("Second");
+        container.add("Third");
+
+        StringBuilder sb = new StringBuilder();
+        for (Object item : container) {
+            sb.append(item).append(" ");
+        }
+
+        assertEquals("First Second Third ", sb.toString(), "Iteration over container should return correct order");
+    }
 }
